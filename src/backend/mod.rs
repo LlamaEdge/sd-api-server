930ca2b4b49a9ac33ee668cd8cd503862decb0ab
Xin Liu <sam@secondstate.io>
2024-11-22 14:07:57 +0800

    chore: improve code
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -4,13 +4,14 @@ use crate::error;
 use hyper::{Body, Request, Response};
 
 pub(crate) async fn handle_sd_request(req: Request<Body>) -> Response<Body> {
+    info!(target: "stdout", "handle llama request: {}", req.uri().path());
+
     match req.uri().path() {
         "/v1/images/generations" => sd::image_generation_handler(req).await,
         "/v1/images/edits" => sd::image_edit_handler(req).await,
         "/v1/images/variations" => sd::image_variation_handler(req).await,
-        "/v1/files" => sd::files_handler(req).await,
         path => {
-            if path.starts_with("/v1/files/") {
+            if path.starts_with("/v1/files") {
                 sd::files_handler(req).await
             } else {
                 error::invalid_endpoint(path)
